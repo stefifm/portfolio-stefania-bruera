316f4625fbadab2b17a42420de5d106ee22c374c
Stefania Veronica Bruera <59149@sistemas.frc.utn.edu.ar>
2024-03-30 20:47:40 -0300

    Add new project

---
+++ src/utils/projects.js
@@ -0,0 +1,54 @@
+import Encriptador from '../assets/encriptador-react.png'
+import RestF1 from '../assets/REST-API-F1.png'
+import Motorflix from '../assets/motorflix.png'
+import Farma from '../assets/FARMASALUD3 E-COMMERCE.png'
+import MotorNext from '../assets/motor-nextjs.png'
+import SqlQuiz from '../assets/sql-quiz.png'
+
+export const projects = [
+  {
+    title: 'SQL Quiz',
+    description:
+      'Bienvenido a SQL Quiz, un pequeño juego de preguntas y respuestas que te permitirá poner a prueba tus conocimientos en SQL. Inspirado en el juego Javascript Quiz y en la clase de React de @midudev',
+    img: SqlQuiz,
+    url: 'https://sql-quiz.surge.sh/',
+    repo: 'https://github.com/stefifm/sql-quiz'
+  },
+  {
+    title: 'MOTORFLIX NextJS',
+    description:
+      'Versión completamente renovada de MotorFlix con NextJS, TailwindCSS, Supabase y NextAuth',
+    img: MotorNext,
+    url: 'https://motorflix-nextjs.vercel.app/',
+    repo: 'https://github.com/stefifm/motorflix-nextjs'
+  },
+  {
+    title: 'Encriptador Web React',
+    description: 'La versión React del primer proyecto de alura "encriptador web"',
+    img: Encriptador,
+    url: 'https://encriptador-react.vercel.app/',
+    repo: 'https://github.com/stefifm/encriptador-react'
+  },
+  {
+    title: 'Rest API F1',
+    description:
+      'Proyecto de la materia Desarrollo de Software de la UTN FRC sobre una REST API de F1',
+    img: RestF1,
+    url: 'https://rest-f1-api-stefifm.vercel.app/',
+    repo: 'https://github.com/stefifm/rest-f1-api-frontend'
+  },
+  {
+    title: 'MotorFlix',
+    description: 'Un CRUD basado en NetFlix pero con vídeos sobre deporte motor. Challenge Alura',
+    img: Motorflix,
+    url: 'https://motorflix-stefifm.vercel.app/',
+    repo: 'https://github.com/stefifm/motorflix'
+  },
+  {
+    title: 'FARMASALUD3 E-Commerce',
+    description: 'CRUD de un e-commerce de farmacia. Challenge Alura',
+    img: Farma,
+    url: 'https://ecommerce-farmasalud3.vercel.app/',
+    repo: 'https://github.com/stefifm/desafio-ecommerce-alura'
+  }
+]
